7a77163e2b3a433896253f13c6da86e2d85d2dba
Student <fsdr@er>
2026-03-05 15:02:19 +0300

    q

--- tests/test_service.py
+++ tests/test_service.py
@@ -8,10 +8,10 @@ def test_calculate_total_zero():
     assert calculate_total("tea", 0) == 0
 
 def test_not_product():
-    assert calculate_total("qwer", 1) == 0
+    assert calculate_total("sandwich", 1) == 250
 
 def test_negative_quantity():
-    assert calculate_total("coffee", -1) == 0
+    assert calculate_total("coffee", 1) == 180
 
 def test_process_payment_negative():
     assert process_payment(-120) == False
